5311a946fde24823bb7ede5fe97ea0e3dc65926a
groiq <35578027+groiq@users.noreply.github.com>
2021-11-12 12:26:08 +0100

    added nullable states

--- athletics-championship.sql
+++ athletics-championship.sql
@@ -1,44 +1,49 @@
+DROP DATABASE IF EXISTS athletics_championship;
+CREATE DATABASE athletics_championship;
+USE athletics_championship;
 
-drop database if exists athletics_championship;
-create database athletics_championship;
-use athletics_championship;
-
-CREATE TABLE nation (
-    nid INT PRIMARY KEY AUTO_INCREMENT,
-    nname VARCHAR(32)
+CREATE TABLE nation
+(
+    nid   INT PRIMARY KEY AUTO_INCREMENT,
+    nname VARCHAR(32) NOT NULL
 );
-CREATE TABLE city (
-    cid INT PRIMARY KEY AUTO_INCREMENT,
-    nid INT,
-    cname VARCHAR(32),
+CREATE TABLE city
+(
+    cid   INT PRIMARY KEY AUTO_INCREMENT,
+    nid   INT         NOT NULL,
+    cname VARCHAR(32) NOT NULL,
     FOREIGN KEY (nid)
         REFERENCES nation (nid)
 );
-CREATE TABLE location (
-    lid INT PRIMARY KEY AUTO_INCREMENT,
-    cid INT,
-    address VARCHAR(64),
+CREATE TABLE location
+(
+    lid     INT PRIMARY KEY AUTO_INCREMENT,
+    cid     INT         NOT NULL,
+    address VARCHAR(64) NOT NULL,
     FOREIGN KEY (cid)
         REFERENCES city (cid)
 );
 
-CREATE TABLE discipline (
-    did INT PRIMARY KEY AUTO_INCREMENT,
-    dname VARCHAR(32)
+CREATE TABLE discipline
+(
+    did   INT PRIMARY KEY AUTO_INCREMENT,
+    dname VARCHAR(32) NOT NULL
 );
 
-CREATE TABLE helper (
-    hid INT PRIMARY KEY AUTO_INCREMENT,
-    hname VARCHAR(64),
-    is_referee BOOLEAN
+CREATE TABLE helper
+(
+    hid        INT PRIMARY KEY AUTO_INCREMENT,
+    hname      VARCHAR(64) NOT NULL,
+    is_referee BOOLEAN     NOT NULL
 );
 
-CREATE TABLE eventx (
-    eid INT PRIMARY KEY AUTO_INCREMENT,
-    referee INT,
-    did INT,
-    lid INT,
-    event_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+CREATE TABLE eventx
+(
+    eid        INT PRIMARY KEY AUTO_INCREMENT,
+    referee    INT COMMENT 'referee (may be decided later)',
+    did        INT       NOT NULL,
+    lid        INT COMMENT 'location (may be decided later)',
+    event_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'may be decided later',
     FOREIGN KEY (referee)
         REFERENCES helper (hid),
     FOREIGN KEY (did)
@@ -47,46 +52,50 @@ CREATE TABLE eventx (
         REFERENCES location (lid)
 );
 
-create table stewardship (
-	eid int,
-    hid int,
-    steward_position varchar(64),
-    foreign key (eid) references eventx (eid),
-    foreign key (hid) references helper (hid),
-    primary key (hid, eid)
+CREATE TABLE stewardship
+(
+    eid              INT,
+    hid              INT,
+    steward_position VARCHAR(64) NOT NULL,
+    FOREIGN KEY (eid) REFERENCES eventx (eid),
+    FOREIGN KEY (hid) REFERENCES helper (hid),
+    PRIMARY KEY (hid, eid)
 );
 
 -- backlog: checks for handling referees and stewards
 
-CREATE TABLE athlete (
-    aid INT PRIMARY KEY AUTO_INCREMENT,
-    aname VARCHAR(64),
-    nid INT,
+CREATE TABLE athlete
+(
+    aid   INT PRIMARY KEY AUTO_INCREMENT,
+    aname VARCHAR(64) NOT NULL,
+    nid   INT         NOT NULL,
     FOREIGN KEY (nid)
         REFERENCES nation (nid)
 );
 
-CREATE TABLE participation (
-    eid INT,
-    aid INT,
-    bib_number INT,
-    UNIQUE (eid , aid),
-    UNIQUE (eid , bib_number),
+CREATE TABLE participation
+(
+    eid        INT,
+    aid        INT,
+    bib_number INT NOT NULL COMMENT 'supposing bib number is decided at registration',
+    UNIQUE (eid, aid),
+    UNIQUE (eid, bib_number),
     FOREIGN KEY (eid)
         REFERENCES eventx (eid),
     FOREIGN KEY (aid)
         REFERENCES athlete (aid),
-    PRIMARY KEY (eid , aid)
+    PRIMARY KEY (eid, aid)
 );
 
-CREATE TABLE result (
-    eid INT,
-    aid INT,
-    score DECIMAL(8 , 2 ),
-    ranking INT,
-    FOREIGN KEY (eid , aid)
-        REFERENCES participation (eid , aid),
-    PRIMARY KEY (eid , aid)
+CREATE TABLE result
+(
+    eid     INT,
+    aid     INT,
+    score   DECIMAL(8, 2) NOT NULL,
+    ranking INT COMMENT 'nullable because only decided at end of event',
+    FOREIGN KEY (eid, aid)
+        REFERENCES participation (eid, aid),
+    PRIMARY KEY (eid, aid)
 );
 
 
